161f88a3d4017f0446e0fb93e8fe439329858b6b
Chloe <chloel.liu@mail.utoronto.ca>
2026-02-25 20:36:22 -0500

    w7

--- src/users.ts
+++ src/users.ts
@@ -7,6 +7,18 @@ export const apiResponse: unknown = [
 ];
 
 export function getUsersData(): User[] {
+  if(Array.isArray(apiResponse)){
+    for (const obj of apiResponse) {
+       if(typeof obj.name !== "string" || typeof obj.age !== "number"){
+        obj.name = String(obj.name);
+        obj.age = Number(obj.age);
+       }
+    }
+  }
+  else{
+    console.log("Invalid data");  
+  }
+  
   return apiResponse as User[]; // intentionally unsafe
 }
 
